49b5689c246c4d538c5d53fde41679c0a8d5e7eb
Elif Merve Eriskin <140524629+emrverskn@users.noreply.github.com>
2024-01-08 15:26:26 +0300

    Updated app.py

--- flask-04-handling-forms-POST-GET-Methods/Flask_GET_POST_Methods/app.py
+++ flask-04-handling-forms-POST-GET-Methods/Flask_GET_POST_Methods/app.py
@@ -2,8 +2,7 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-
-# create a function named "lcm" which calculates a least common multiple values of two numbers. 
+ 
 def lcm(num1,num2):
     common_multiplications = [] 
     for i in range(max(num1, num2),num1*num2+1):
